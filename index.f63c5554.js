61ba23d60f5b6b7ac95002c14eb36a87227d25d1
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-27 18:06:07 +0300

    make build

---
+++ index.f63c5554.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var l=e.cells[1].cloneNode(!0);e.insertBefore(l,e.cells[e.cells.length-1])});
+//# sourceMappingURL=index.f63c5554.js.map
